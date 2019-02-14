584b8899d52de7e28afbeee48deb9a7da6118992
Daniel <danielyung211@gmail.com>
2019-02-14 17:54:58 +0000

    pseudo code for listen in replica

--- multipaxos/lib/multipaxos.ex
+++ multipaxos/lib/multipaxos.ex
@@ -5,38 +5,36 @@
 
 defmodule Multipaxos do
 
-def main do 
-  config = DAC.get_config
-  if config.debug_level >= 2, do: IO.inspect config
-  if config.setup == :docker, do: Process.sleep config.docker_delay
-  start config
-end
-
-defp start config do
-  monitor = spawn Monitor, :start, [config] 
-  config  = Map.put config, :monitor, monitor
-
-  for s <- 1 .. config.n_servers do 
-    node_name = DAC.node_name config, "server", s
-    DAC.node_spawn node_name, Server, :start, [config, s, self(), monitor] 
+  def main do 
+    config = DAC.get_config
+    if config.debug_level >= 2, do: IO.inspect config
+    if config.setup == :docker, do: Process.sleep config.docker_delay
+    start config
   end
 
-  server_components = 
-    for _ <- 1 .. config.n_servers do 
-      receive do { :config, replica, acceptor, leader } -> { replica, acceptor, leader } end 
+  defp start config do
+    monitor = spawn Monitor, :start, [config] 
+    config  = Map.put config, :monitor, monitor
+
+    for s <- 1 .. config.n_servers do 
+      node_name = DAC.node_name config, "server", s
+      DAC.node_spawn node_name, Server, :start, [config, s, self(), monitor] 
     end
 
-  { replicas, acceptors, leaders } = DAC.unzip3 server_components
+    server_components = 
+      for _ <- 1 .. config.n_servers do 
+        receive do { :config, replica, acceptor, leader } -> { replica, acceptor, leader } end 
+      end
 
-  for replica <- replicas, do: send replica, { :bind, leaders }
-  for leader  <- leaders,  do: send leader,  { :bind, acceptors, replicas }
+    { replicas, acceptors, leaders } = DAC.unzip3 server_components
 
-  for c <- 1 .. config.n_clients do 
-    node_name = DAC.node_name config, "client", c
-    DAC.node_spawn node_name, Client, :start, [config, c, replicas] 
-  end
+    for replica <- replicas, do: send replica, { :replica_bind_leader, leaders }
+    for leader  <- leaders,  do: send leader,  { :leader_bind_acc_repl, acceptors, replicas }
 
-end # start
-
+    for c <- 1 .. config.n_clients do 
+      node_name = DAC.node_name config, "client", c
+      DAC.node_spawn node_name, Client, :start, [config, c, replicas] 
+    end
+  end # start
 end # Multipaxos
 
--- multipaxos/lib/replica.ex
+++ multipaxos/lib/replica.ex
@@ -2,6 +2,48 @@
 
 defmodule Replica do
   def start(config, database, monitor) do
-    
+    initial_state = %{
+      clients: MapSet.new(),
+      slot_in: 1,
+      slot_out: 1,
+      requests: MapSet.new(),
+      proposals: MapSet.new(),
+      decision: MapSet.new()
+    }
+
+    receive do
+      { :replica_bind_leader, leaders } ->
+        listen(leaders, initial_state)
+    end
+  end
+
+  def propose(state) do
+    window = state.window
+
+
+  end
+
+  def perform({ client, cid, op }) do
+
+  end
+
+  def listen(leaders, state) do
+    receive do
+      { :request, client} ->
+        propose(leaders, MapSet.put(clients, client))
+      { :decision, slot_number, cmd } ->
+        decision = Map.get(state, :decision)
+        decision = MapSet.put(decision, { slot_number, cmd })
+        state = Map.put(state, :decision, decision)
+        # loop through decision to find a c' that has the same slot number as the cur slot_out in state
+        # for each of the command with the same slot number:
+          # for each of pair { slot_num, c''} in the proposals
+            # remove the pair
+            # check if c' == c''
+              # add the c'' into request
+            #
+          #
+          # perform(c')
+    end
   end
 end
