b1c978e895b2166c469e74f3a87908a8649edf18
Daniel <danielyung211@gmail.com>
2019-02-14 16:41:22 +0000

    added recursion

--- multipaxos/lib/acceptor.ex
+++ multipaxos/lib/acceptor.ex
@@ -8,14 +8,16 @@ defmodule Acceptor do
     receive do
       { :p1a, sender, ballot_suggest } ->
         if ballot_suggest > curr_ballot do
-          listen()
+          curr_ballot = ballot_suggest
         end
         send sender, { :p1b, self(), curr_ballot, accepted}
+        listen(config, curr_ballot, accepted)
       { :p2a, sender, { ballot_suggest, slot_number, command} = package} ->
         if ballot_suggest == curr_ballot do
           listen(config, curr_ballot, MapSet.put(accepted, package))
         else
           send sender, { :p2b, self(), curr_ballot }
+          listen(config, curr_ballot, accepted)
         end
     end
   end
